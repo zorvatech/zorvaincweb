4fb59ea9044f7454353a580dd2805217ccacb3da
Mohammed <mohanadam2233@gmail.com>
2026-08-31 20:12:55 +0300

    added user schema

--- server/modules/User.js
+++ server/modules/User.js
@@ -1,12 +1,24 @@
 import mongoose  from "mongoose";
-
-
-
+import bcrypt from"bcrypt";
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: { type: String, required: true, unique: true, lowercase: true,trim: true},
-
+    password:{type:String,rquired:true},
+    role:{type:String,required:true,enum:["admin","User"], default:"User"},
+    isactive:{Boolean,default:flase}
 },{timestamps:true})
 
+//hasshing password
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password"))return next();
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt);
+    
+})
 
-export default userSchema;
+//mathces password
+userSchema.mehthods.matchPassword=async function(entetrdPassword){
+    return await bcrypt.compare(entetrdPassword,this.password);
+}
+
+export default mongoose.model("Users",userSchema);
